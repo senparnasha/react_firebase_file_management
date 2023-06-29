19dfa4acc824b3f3d0d03e6ebcf7a3492a9f61ff
senparnasha <senparnasha@gmail.com>
2023-06-29 23:11:41 +0530

    10th push

--- src/components/HomepageComponents/DashboardComponents/ShowItems/ShowItems.js
+++ src/components/HomepageComponents/DashboardComponents/ShowItems/ShowItems.js
@@ -2,10 +2,21 @@ import React from "react";
 import "./ShowItems.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileAlt, faFolder } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 const ShowItems = ({ title, items, type }) => {
 
-  console.log("tan=>", items[0].data);
+const navigate=useNavigate();
+
+  const handleDbClick=(itemId)=>{
+if(type==="folder"){
+  navigate(`/dashboard/folder/${itemId}`);
+}else{
+  alert("File clicked")
+}
+  }
+
+
   return (
     <div className="w-100">
       <h4 className="text-center border-bottom py-2">{title}</h4>
@@ -15,6 +26,7 @@ const ShowItems = ({ title, items, type }) => {
             <p
               key={index * 55}
               className="col-md-2 py-3 text-center d-flex flex-column border"
+              onDoubleClick={()=>handleDbClick(item.docId)}
             >
               {type === "folder" ? (
                 <FontAwesomeIcon icon={faFolder} size="4x" className="mb-3" />
